f82c0eff2d1f974cd9d3a989b4e7d80257bbedc1
Viktar_Pryshchepa <viktar_pryshchepa@epam.com>
2015-10-06 19:39:43 +0300

    second

--- js/com/script.js
+++ js/com/script.js
@@ -4,36 +4,52 @@ app.controller("myMainController", ['$scope', function($scope) {
   $scope.message = "AAAA";
 }]);
 
-app.controller('pictureController', ['$scope', function ($scope) {
+app.controller('pictureController', ['$scope','$filter', function ($scope, $filter) {
+  $scope.name_filter = '';
+  $scope._query = '';
+  $scope.sortOrder = false;
   $scope.mediaList = [
     {
       click: 0,
       imagePath: "../../img/400_400.jpg",
-      name: 'First'
+      name: 'First',
+      viewed: false,
+      raiting: 0
     },
     {
       click: 0,
       imagePath: "../../img/400_400_1.jpg",
-      name: 'Second'
+      name: 'Second',
+      viewed: false,
+      raiting: 0
     },
     {
       click: 0,
       imagePath: "../../img/400_400_2.jpg",
-      name: 'Third'
+      name: 'Third',
+      viewed: false,
+      raiting: 0
     },
     {
       click: 0,
       imagePath: "../../img/400_400_3.jpg",
-      name: 'Fourth'
+      name: 'Fourth',
+      viewed: false,
+      raiting: 0
     },
     {
       click: 0,
       imagePath: "../../img/400_400_4.jpg",
-      name: 'Fifth'
+      name: 'Fifth',
+      viewed: false,
+      raiting: 0
     },
   ];
-  $scope.select = function(ind){
-    $scope.selected = $scope.mediaList[ind];
+  $scope.select = function(media){
+    var index =  $scope.mediaList.indexOf(media);
+    console.log(index);
+    $scope.mediaList[index].viewed = '✓';
+    $scope.selected = $scope.mediaList[index];
   }
 
   $scope.inc = function (selected) {
@@ -41,4 +57,16 @@ app.controller('pictureController', ['$scope', function ($scope) {
     $scope.selected = selected;
   }
 
+  $scope.plusRaiting = function (selected) {
+    selected.raiting++;
+    $scope.selected = selected;
+  }
+  $scope.minusRaiting = function (selected) {
+    selected.raiting--;
+    $scope.selected = selected;
+  }
+  $scope.applySearch = function () {
+    $scope._query = $scope.query;
+    console.log($scope.sortOrder);
+  }
 }]);
